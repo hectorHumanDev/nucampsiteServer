9346fc57191f1f8536330e605bf3b3cc4750c0e1
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2024-05-30 13:24:07 -0700

    Express-session, FileStore

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -28,10 +30,21 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+//app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false, //prevent saving empty cookies
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       const err = new Error("You are not authenticated");
@@ -48,7 +61,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === "admin" && pass === "password") {
-      res.cookie("user", "admin", { signed: true }); //{signed: true} --> let's express know to use the secret key from cookie-parser 'cookieParser("12345-67890-09876-54321")' to sign cookie
+      req.session.user = "admin";
       return next(); //user has been authorized
     } else {
       const err = new Error("Your are not authenticated");
@@ -57,7 +70,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       return next();
     } else {
       const err = new Error("Your are not authenticated");
